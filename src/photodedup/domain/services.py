6a52499bead1eee1d1b9de849a9f50e1825d0e9c
AlkakoS <alkakos.code@gmail.com>
2026-02-16 14:19:20 +0100

    fix: correction du typage des certaines functions et try/except dans find_exact_duplicates

--- src/photodedup/domain/services.py
+++ src/photodedup/domain/services.py
@@ -1,9 +1,11 @@
 from collections import defaultdict
+from pathlib import Path
+from typing import Callable
 
-from photodedup.domain.models import DuplicateGroup
+from photodedup.domain.models import DuplicateGroup, ImageFile
 
 
-def group_by_size(imagefiles: list) -> dict:
+def group_by_size(imagefiles: list[ImageFile]) -> dict[int, list[ImageFile]]:
     images_grouped_by_size = defaultdict(list)
 
     for image in imagefiles:
@@ -15,18 +17,18 @@ def remove_singletons(grouped_images: dict) -> dict:
     return {k: v for k, v in grouped_images.items() if len(v) > 1}
 
 
-def find_exact_duplicates(images, hasher) -> list[DuplicateGroup]:
+def find_exact_duplicates(images, hasher: Callable[[Path], str]) -> list[DuplicateGroup]:
     result = []
     grouped_images_by_size = group_by_size(images)
 
     grouped_by_hash = defaultdict(list)
     for size, imagegroup in grouped_images_by_size.items():
-        try:
-            for image in imagegroup:
-                hash = hasher(image.path)
-                grouped_by_hash[hash].append(image)
-        except OSError:
-            continue
+        for image in imagegroup:
+            try:
+                h = hasher(image.path)
+                grouped_by_hash[h].append(image)
+            except OSError:
+                continue
 
     for hash, group in grouped_by_hash.items():
         if len(group) >= 2:
